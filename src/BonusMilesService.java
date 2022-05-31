15263580ecc08c1966073001f522df25c94173f3
tomaslinkina <altoma@mail.ru>
2022-05-31 20:57:20 +0500

    Reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
-    public int calculate (int cost) {
+    public int calculate(int cost) {
         int result;
         if (cost > 20) {
-            result = cost/20;
+            result = cost / 20;
         } else {
             result = 0;
         }
